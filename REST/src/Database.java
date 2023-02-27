10d47f749eb9d95cff799dcac6d820686f928d5b
geniusfoever <58506698+geniusfoever@users.noreply.github.com>
2023-02-27 14:35:23 +0800

    我运行不了，所以可能很多错，你看看你能否运行

--- REST/src/Database.java
+++ REST/src/Database.java
@@ -1,6 +1,7 @@
 import java.util.Hashtable;
 
-import javax.ws.rs.Path;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
 
 import com.sun.jersey.spi.resource.Singleton;
 
@@ -9,18 +10,37 @@ import com.sun.jersey.spi.resource.Singleton;
 public class Database {
 	private Hashtable<String, Integer> table = new Hashtable<String, Integer>();
 
-	public String addRecord() {
-// Add a record using id as key and mark as value
-		return null;
+	@POST
+	@Path("/add")
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	public String addRecord(@FormParam("id") String id, @FormParam("mark") int mark) {
+		// Add a record using id as key and mark as value
+		table.put(id, mark);
+		return "Record added successfully";
 	}
 
-	public String getMark() {
-// Retrieve a record using id as key
-		return null;
+	@GET
+	@Path("/get/{id}")
+	@Produces(MediaType.TEXT_PLAIN)
+	public String getMark(@PathParam("id") String id) {
+		// Retrieve a record using id as key
+		Integer mark = table.get(id);
+		if (mark == null) {
+			return "Record not found";
+		} else {
+			return Integer.toString(mark);
+		}
 	}
 
-	public String updateRecord() {
-// Update a record using id as key and mark as value
-		return null;
+	@PUT
+	@Path("/update")
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	public String updateRecord(@FormParam("id") String id, @FormParam("mark") int mark) {
+		// Update a record using id as key and mark as value
+		if (table.containsKey(id)) {
+			table.put(id, mark);
+			return "Record updated successfully";
+		} else {
+			return "Record not found";
+		}
 	}
-}
